90fe827937939a8997616639e7ce971ce24d25da
Ali Ahangar <aliahangar80@gmail.com>
2025-10-21 15:24:45 +0330

    Initial commit for the Number Guessing Game.

---
+++ number_guessing_game.py
@@ -0,0 +1,15 @@
+import random
+
+choice = input("Guess the number between 1 and 100: ")
+random_value = random.randint(1, 100)
+
+while True:
+    if choice < random_value:
+        print("Too low!")
+    elif choice > random_value:
+        print("Too high!")
+    elif choice == random_value:
+        print("Congradulations! You guessed the number.")
+        break
+    else:
+        print("Please enter a valid number.")
