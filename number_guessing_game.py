c4d40978227c393adea31292c8d3e130e3727a3a
Ali Ahangar <aliahangar80@gmail.com>
2025-10-21 15:44:28 +0330

    Add try block for handling value error

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,13 +2,14 @@ import random
 
 random_value = random.randint(1, 100)
 while True:
-    choice = int(input("Guess the number between 1 and 100: "))
-    if choice < random_value:
-        print("Too low!")
-    elif choice > random_value:
-        print("Too high!")
-    elif choice == random_value:
-        print("Congradulations! You guessed the number.")
-        break
-    else:
+    try:
+        choice = int(input("Guess the number between 1 and 100: "))
+        if choice < random_value:
+            print("Too low!")
+        elif choice > random_value:
+            print("Too high!")
+        elif choice == random_value:
+            print("Congradulations! You guessed the number.")
+            break
+    except ValueError:
         print("Please enter a valid number.")
